8f50704a0cc2f3eaa358e0ae125558318747e392
v-like-engine <cypepv@yandex.ru>
2022-06-04 23:28:28 +0300

    Implement image clearing & saving in `cg::renderer::rasterization_renderer` class

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -5,7 +5,16 @@
 
 void cg::renderer::rasterization_renderer::init()
 {
-	// TODO: Lab 1.02. Implement image clearing & saving in `cg::renderer::rasterization_renderer` class
+	render_target = std::make_shared<cg::resource<cg::unsigned_color>>(
+			settings->width, settings->height
+	);
+	rasterizer = std::make_shared<
+			cg::renderer::rasterizer<cg::vertex, cg::unsigned_color>>();
+	rasterizer->set_viewport(settings->width, settings->height);
+	rasterizer->set_render_target(render_target);
+
+	model = std::make_shared<cg::world::model>();
+	model->load_obj(settings->model_path);
 	// TODO: Lab 1.03. Adjust `cg::renderer::rasterization_renderer` class to consume `cg::world::model`
 	// TODO: Lab 1.04. Setup an instance of camera `cg::world::camera` class in `cg::renderer::rasterization_renderer`
 	// TODO: Lab 1.06. Add depth buffer in cg::renderer::rasterization_renderer
