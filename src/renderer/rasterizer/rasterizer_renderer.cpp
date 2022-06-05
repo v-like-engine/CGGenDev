2126ed46a18e5bb102097514659c22f11fc32e17
v-like-engine <cypepv@yandex.ru>
2022-06-06 00:14:35 +0300

    Implement `cg::world::camera` class

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -15,7 +15,6 @@ void cg::renderer::rasterization_renderer::init()
 
 	model = std::make_shared<cg::world::model>();
 	model->load_obj(settings->model_path);
-	// TODO: Lab 1.03. Adjust `cg::renderer::rasterization_renderer` class to consume `cg::world::model`
 	// TODO: Lab 1.04. Setup an instance of camera `cg::world::camera` class in `cg::renderer::rasterization_renderer`
 	// TODO: Lab 1.06. Add depth buffer in cg::renderer::rasterization_renderer
 	
@@ -29,7 +28,6 @@ void cg::renderer::rasterization_renderer::render()
 		rasterizer->set_index_buffer((model->get_index_buffers()[shape_id]));
 		rasterizer->draw(model->get_index_buffers()[shape_id]->get_number_of_elements(), 0);
 	}
-	// TODO: Lab 1.03. Adjust `cg::renderer::rasterization_renderer` class to consume `cg::world::model`
 	// TODO: Lab 1.04. Implement `vertex_shader` lambda for the instance of `cg::renderer::rasterizer`
 	// TODO: Lab 1.05. Implement `pixel_shader` lambda for the instance of `cg::renderer::rasterizer`
 	cg::utils::save_resource(*render_target, settings->result_path);
