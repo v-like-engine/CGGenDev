edb65b57bfcc3c29f3f4e712b9147484305757e5
v-like-engine <cypepv@yandex.ru>
2022-06-06 17:13:46 +0300

    Setup an instance of camera `cg::world::camera` class in `cg::renderer::rasterization_renderer`

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -15,7 +15,22 @@ void cg::renderer::rasterization_renderer::init()
 
 	model = std::make_shared<cg::world::model>();
 	model->load_obj(settings->model_path);
-	// TODO: Lab 1.04. Setup an instance of camera `cg::world::camera` class in `cg::renderer::rasterization_renderer`
+
+	camera = std::make_shared<cg::world::camera>();
+	camera->set_height(static_cast<float>(settings->height));
+	camera->set_width(static_cast<float>(settings->width));
+	camera->set_position(
+			float3{
+					settings->camera_position[0],
+					settings->camera_position[1],
+					settings->camera_position[2]
+			}
+			);
+	camera->set_phi(settings->camera_phi);
+	camera->set_theta(settings->camera_theta);
+	camera->set_angle_of_view(settings->camera_angle_of_view);
+	camera->set_z_near(settings->camera_z_near);
+	camera->set_z_far(settings->camera_z_far);
 	// TODO: Lab 1.06. Add depth buffer in cg::renderer::rasterization_renderer
 	
 }
