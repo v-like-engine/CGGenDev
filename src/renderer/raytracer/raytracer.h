59865e142ac603bce3be79635b18f51fd2e90d1a
v-like-engine <cypepv@yandex.ru>
2022-06-23 16:25:50 +0300

    Implement build_acceleration_structure method of raytracer class

--- src/renderer/raytracer/raytracer.h
+++ src/renderer/raytracer/raytracer.h
@@ -170,16 +170,17 @@ namespace cg::renderer
 			auto& index_buffer = index_buffers[shape_id];
 			auto& vertex_buffer = vertex_buffers[shape_id];
 			size_t index_id = 0;
+			aabb<VB> aabb;
 			while(index_id < index_buffer->get_number_of_elements())
 			{
 				triangle<VB> triangle(
 						vertex_buffer->item(index_buffer->item(index_id++)),
 						vertex_buffer->item(index_buffer->item(index_id++)),
 						vertex_buffer->item(index_buffer->item(index_id++)));
-				triangles.push_back(triangle);
+				aabb.add_triangle(triangle);
 			}
+			acceleration_structures.push_back(aabb);
 		}
-		// TODO: Lab 2.05. Implement build_acceleration_structure method of raytracer class
 	}
 
 	template<typename VB, typename RT>
