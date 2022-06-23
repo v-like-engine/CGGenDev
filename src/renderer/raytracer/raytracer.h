9a611856a85d11643a45ca8131cf17392326ba7e
v-like-engine <cypepv@yandex.ru>
2022-06-23 16:28:51 +0300

    Adjust trace_ray method of raytracer class to traverse the acceleration structure

--- src/renderer/raytracer/raytracer.h
+++ src/renderer/raytracer/raytracer.h
@@ -228,16 +228,21 @@ namespace cg::renderer
 		closest_hit_payload.t = max_t;
 		const triangle<VB>* closest_triangle = nullptr;
 
-		for (auto& triangle : triangles)
+		for (auto& aabb: acceleration_structures)
 		{
-			payload payload = intersection_shader(triangle, ray);
-			if (payload.t > min_t && payload.t < closest_hit_payload.t)
+			if (!aabb.aabb_test(ray))
+				continue;
+			for (auto& triangle: aabb.get_triangles())
 			{
-				closest_hit_payload = payload;
-				closest_triangle = &triangle;
+				payload payload = intersection_shader(triangle, ray);
+				if (payload.t > min_t && payload.t < closest_hit_payload.t)
+				{
+					closest_hit_payload = payload;
+					closest_triangle = &triangle;
 
-				if (any_hit_shader)
-					return any_hit_shader(ray, payload, triangle);
+					if (any_hit_shader)
+						return any_hit_shader(ray, payload, triangle);
+				}
 			}
 		}
 
@@ -249,7 +254,6 @@ namespace cg::renderer
 		}
 
 		return miss_shader(ray);
-		// TODO: Lab 2.05. Adjust trace_ray method of raytracer class to traverse the acceleration structure
 	}
 
 	template<typename VB, typename RT>
