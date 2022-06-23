e4f96bf6792b050f2a7b57cb7fb784ed7ef3a7d0
v-like-engine <cypepv@yandex.ru>
2022-06-23 16:46:09 +0300

    Add `history` resource in `raytracer` class

--- src/renderer/raytracer/raytracer.h
+++ src/renderer/raytracer/raytracer.h
@@ -148,8 +148,8 @@ namespace cg::renderer
 		for (size_t i = 0; i < render_target->get_number_of_elements(); i++)
 		{
 			render_target->item(i) = in_clear_value;
+			history->item(i) = float3{0.f, 0.f, 0.f};
 		}
-		// TODO: Lab 2.06. Add `history` resource in `raytracer` class
 	}
 	template<typename VB, typename RT>
 	void raytracer<VB, RT>::set_index_buffers(std::vector<std::shared_ptr<cg::resource<unsigned int>>> in_index_buffers)
@@ -189,7 +189,7 @@ namespace cg::renderer
 	{
 		height = in_height;
 		width = in_width;
-		// TODO: Lab 2.06. Add `history` resource in `raytracer` class
+		history = std::make_shared<cg::resource<float3>>(width, height);
 	}
 
 	template<typename VB, typename RT>
