8548adb24c76189f8661ce5e88391836c994271b
v-like-engine <cypepv@yandex.ru>
2022-06-23 19:05:15 +0300

    Map resources and copy suitable data to the resources

--- src/renderer/dx12/dx12_renderer.cpp
+++ src/renderer/dx12/dx12_renderer.cpp
@@ -192,7 +192,14 @@ void cg::renderer::dx12_renderer::create_resource_on_default_heap(ComPtr<ID3D12R
 
 void cg::renderer::dx12_renderer::copy_data(const void* buffer_data, UINT buffer_size, ComPtr<ID3D12Resource>& destination_resource)
 {
-	// TODO Lab 3.03. Map resources and copy suitable data to the resources
+	UINT8* buffer_data_begin;
+	CD3DX12_RANGE read_range(0, 0);
+	THROW_IF_FAILED(
+			destination_resource->Map(0, &read_range,
+							  reinterpret_cast<void**>(&buffer_data_begin)));
+
+	memcpy(buffer_data_begin, buffer_data, buffer_size);
+	destination_resource->Unmap(0, 0);
 }
 
 void cg::renderer::dx12_renderer::copy_data(const void* buffer_data, const UINT buffer_size, ComPtr<ID3D12Resource>& destination_resource, ComPtr<ID3D12Resource>& intermediate_resource, D3D12_RESOURCE_STATES state_after, int row_pitch, int slice_pitch)
@@ -262,7 +269,7 @@ void cg::renderer::dx12_renderer::load_assets()
 	}
 
 	// Constant buffer
-	std::wstring const_buffer_name(L"Constant buffer ");
+	std::wstring const_buffer_name(L"Constant buffer");
 	create_resource_on_upload_heap(constant_buffer,
 								   64 * 1024,
 								   const_buffer_name);
