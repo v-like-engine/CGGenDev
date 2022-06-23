6382385a6353722976f25fa867b6c0c5657411de
v-like-engine <cypepv@yandex.ru>
2022-06-23 17:53:25 +0300

    Enumerate hardware adapters

--- src/world/camera.cpp
+++ src/world/camera.cpp
@@ -93,6 +93,7 @@ const DirectX::XMMATRIX cg::world::camera::get_dxm_projection_matrix() const
 const DirectX::XMMATRIX camera::get_dxm_mvp_matrix() const
 {
 	// TODO Lab 3.08. Implement `get_dxm_view_matrix`, `get_dxm_projection_matrix`, and `get_dxm_mvp_matrix` methods of `camera`
+	return DirectX::XMMatrixIdentity();
 }
 #endif
 
