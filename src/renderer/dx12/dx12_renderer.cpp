ba25e62e6d369c5504620c5e0b4620b67c82e062
v-like-engine <cypepv@yandex.ru>
2022-07-07 15:58:58 +0300

    Setup a PSO descriptor and create a PSO

--- src/renderer/dx12/dx12_renderer.cpp
+++ src/renderer/dx12/dx12_renderer.cpp
@@ -265,7 +265,69 @@ ComPtr<ID3DBlob> cg::renderer::dx12_renderer::compile_shader(const std::filesyst
 
 void cg::renderer::dx12_renderer::create_pso(const std::string& shader_name)
 {
-	// TODO Lab 3.05. Setup a PSO descriptor and create a PSO
+
+	// Compile shaders
+	ComPtr<ID3DBlob> vertex_shader = compile_shader(
+			get_shader_path(shader_name),
+			"VSMain",
+			"vs_5_0"
+			);
+	ComPtr<ID3DBlob> pixel_shader = compile_shader(
+			get_shader_path(shader_name),
+			"PSMain",
+			"ps_5_0"
+			);
+
+	// Input element description
+	D3D12_INPUT_ELEMENT_DESC input_descriptors[] = {
+			{"POSITION", 0,
+			DXGI_FORMAT_R32G32B32_FLOAT,
+			0, 0,
+			D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0},
+			{"NORMAL", 0,
+			 DXGI_FORMAT_R32G32B32_FLOAT,
+			 0, 12,
+			 D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0},
+			{"TEXCOORD", 0,
+			 DXGI_FORMAT_R32G32_FLOAT,
+			 0, 24,
+			 D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0},
+			{"COLOR", 0,
+			 DXGI_FORMAT_R32G32B32_FLOAT,
+			 0, 32,
+			 D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0},
+			{"COLOR", 1,
+			 DXGI_FORMAT_R32G32B32_FLOAT,
+			 0, 44,
+			 D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0},
+			{"COLOR", 2,
+			 DXGI_FORMAT_R32G32B32_FLOAT,
+			 0, 56,
+			 D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0}
+	};
+
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC pso_desc = {};
+	pso_desc.InputLayout = {
+			input_descriptors,
+			_countof(input_descriptors)};
+	pso_desc.pRootSignature = root_signature.Get();
+	pso_desc.VS = CD3DX12_SHADER_BYTECODE(vertex_shader.Get());
+	pso_desc.VS = CD3DX12_SHADER_BYTECODE(pixel_shader.Get());
+	pso_desc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
+	pso_desc.RasterizerState.FrontCounterClockwise = TRUE;
+	pso_desc.RasterizerState.FillMode = D3D12_FILL_MODE_SOLID;
+	pso_desc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
+	pso_desc.DepthStencilState.DepthEnable = FALSE;
+	pso_desc.DepthStencilState.StencilEnable = FALSE;
+	pso_desc.SampleMask = UINT_MAX;
+	pso_desc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+	pso_desc.NumRenderTargets = 1;
+	pso_desc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
+	pso_desc.SampleDesc.Count = 1;
+
+	THROW_IF_FAILED(device->CreateGraphicsPipelineState(
+			&pso_desc,
+			IID_PPV_ARGS(&pipeline_state)));
 }
 
 void cg::renderer::dx12_renderer::create_resource_on_upload_heap(ComPtr<ID3D12Resource>& resource, UINT size, const std::wstring& name)
