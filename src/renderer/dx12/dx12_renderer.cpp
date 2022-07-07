7cf09d3520a6defde1aa1cb0285a3f71cfa42131
v-like-engine <cypepv@yandex.ru>
2022-07-07 14:49:18 +0300

    Create index buffer views

--- src/renderer/dx12/dx12_renderer.cpp
+++ src/renderer/dx12/dx12_renderer.cpp
@@ -235,8 +235,10 @@ D3D12_VERTEX_BUFFER_VIEW cg::renderer::dx12_renderer::create_vertex_buffer_view(
 
 D3D12_INDEX_BUFFER_VIEW cg::renderer::dx12_renderer::create_index_buffer_view(const ComPtr<ID3D12Resource>& index_buffer, const UINT index_buffer_size)
 {
-	// TODO Lab 3.04. Create index buffer views
 	D3D12_INDEX_BUFFER_VIEW view{};
+	view.BufferLocation = index_buffer->GetGPUVirtualAddress();
+	view.SizeInBytes = index_buffer_size;
+	view.Format = DXGI_FORMAT_R32_UINT;
 	return view;
 }
 
@@ -298,6 +300,11 @@ void cg::renderer::dx12_renderer::load_assets()
 		copy_data(index_buffer_data->get_data(),
 				  index_buffer_size,
 				  index_buffers[i]);
+
+		index_buffer_views[i] = create_index_buffer_view(
+				index_buffers[i],
+				index_buffer_size
+				);
 	}
 
 	// Constant buffer
